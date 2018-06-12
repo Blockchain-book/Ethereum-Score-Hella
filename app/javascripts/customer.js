557830cfadd6418cdde02ecba85f7e007e498395
sunlggggg <sunlggggg@gmail.com>
2018-06-12 15:09:31 +0800

    基本完成重写

--- app/javascripts/customer.js
+++ app/javascripts/customer.js
@@ -31,7 +31,6 @@ module.exports = {
   customerLogin: function (ScoreInstance, account) {
     const address = document.getElementById('customerLoginAddr').value
     const password = document.getElementById('customerLoginPwd').value
-
     ScoreInstance.getCustomerPassword(address, { from: account, gas: 3000000 }).then(function (result) {
       if (result[0]) {
         // 查询密码成功
@@ -49,5 +48,54 @@ module.exports = {
         window.alert('该用户不存在，请确定账号后再登录！')
       }
     })
+  },
+  getScoreWithCustomerAddr: function (currentAccount, ScoreInstance, account) {
+    ScoreInstance.getScoreWithCustomerAddr.call(currentAccount, { from: account }).then(function (value) {
+      window.alert('当前余额：' + value.valueOf())
+    }).catch(function (e) {
+      console.log(e)
+      window.alert('出现异常，查询余额失败！')
+    })
+  },
+  getGoodsByCustomer: function (currentAccount, ScoreInstance, account) {
+    ScoreInstance.getGoodsByCustomer.call(currentAccount, { from: account }).then(function (result) {
+      if (result.length === 0) {
+        window.App.setStatus('空...')
+      } else {
+        let goods = ''
+        for (let i = 0; i < result.length; i++) {
+          const temp = utils.hexCharCodeToStr(result[i])
+          console.log(temp)
+          goods += ',' + temp
+        }
+        window.App.setStatus(goods)
+      }
+    })
+  },
+  // 客户实现任意的积分转让
+  transferScoreToAnotherFromCustomer: function (currentAccount, ScoreInstance, account) {
+    const receivedAddr = document.getElementById('anotherAddress').value
+    const amount = parseInt(document.getElementById('scoreAmount').value)
+    ScoreInstance.transferScoreToAnother(0, currentAccount, receivedAddr, amount, { from: account })
+    ScoreInstance.TransferScoreToAnother(function (e, r) {
+      if (!e) {
+        console.log(r.args)
+        window.App.setStatus(r.args.message)
+      }
+    })
+  },
+  buyGood: function (currentAccount, ScoreInstance, account) {
+    const goodId = document.getElementById('goodId').value
+    ScoreInstance.buyGood(currentAccount, goodId, { from: account, gas: 1000000 }).then(function () {
+      ScoreInstance.BuyGood(function (error, event) {
+        if (!error) {
+          console.log(event.args.message)
+          window.App.setStatus(event.args.message)
+        }
+      })
+    })
+  },
+  showCurrentAccount: function (currentAccount) {
+    window.alert(currentAccount)
   }
 }
