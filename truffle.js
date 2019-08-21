7570199950c858423058c02e2d9f06aebea3f896
sunlggggg <sunlggggg@gmail.com>
2019-08-21 14:26:43 +0800

    feat: 端口更改

--- truffle.js
+++ truffle.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     truffle: {
       host: '127.0.0.1',
-      port: 9545,
+      port: 8545,
       network_id: '*' // Match any network id
     },
     develop: {
